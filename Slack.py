01c4e070bd0ea7ed5928d6d105ee7a4167f48d8b
kagunda10 <caleb.njiiri@gmail.com>
2019-03-22 08:42:40 +0300

    Code refactoring

--- Slack.py
+++ Slack.py
@@ -11,38 +11,40 @@ webhook_ = config.slackConfig.WEBHOOK
 
 def get_channel_id():
     channel_list = sc.api_call("channels.list")
-    for each in channel_list['channels']:
-        if each['name'] == 'general':
-            return each['id']
+    for each in channel_list["channels"]:
+        if each["name"] == "general":
+            return each["id"]
+
 
 # Post data to slack channel
 def post(item):
-    item_name = item['title']
-    item_link = item['link']
-    item_price = item['price']
-    item_img = "{}{}".format("http://",item['img'][:-2])
-    attachments = [{"title": item_name,
-        "title_link":item_link,
-        "fields": [{
-            "title": "Retailer",
-            "value": "Funko Shop"},{
-            "title": "Price",
-            "value": item_price},{
-            "title": "Link",
-            "value": item_link
-            }
-        ],
-        "image_url": item_img }]
+    item_name = item["title"]
+    item_link = item["link"]
+    item_price = item["price"]
+    item_img = "{}{}".format("http://", item["img"][:-2])
+    attachments = [
+        {
+            "title": item_name,
+            "title_link": item_link,
+            "fields": [
+                {"title": "Retailer", "value": "Funko Shop"},
+                {"title": "Price", "value": item_price},
+                {"title": "Link", "value": item_link},
+            ],
+            "image_url": item_img,
+        }
+    ]
     # Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
     webhook_url = webhook_
     slack_data = attachments
 
     response = requests.post(
-        webhook_url, data=json.dumps(slack_data),
-        headers={'Content-Type': 'application/json'}
+        webhook_url,
+        data=json.dumps(slack_data),
+        headers={"Content-Type": "application/json"},
     )
     if response.status_code != 200:
         raise ValueError(
-            'Request to slack returned an error %s, the response is:\n%s'
+            "Request to slack returned an error %s, the response is:\n%s"
             % (response.status_code, response.text)
-    )
+        )
